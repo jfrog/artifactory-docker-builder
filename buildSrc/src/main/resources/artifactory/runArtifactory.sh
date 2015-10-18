d7f16e515ac617ec4ff645efd25ac3ff49dc18e1
matank <matank@jfrog.com>
2015-10-18 14:58:49 +0300

    Add distribution flag to JAVA_OPTIONS

--- buildSrc/src/main/resources/artifactory/runArtifactory.sh
+++ buildSrc/src/main/resources/artifactory/runArtifactory.sh
@@ -3,4 +3,9 @@ service nginx start
 if [ -n "$RUNTIME_OPTS" ]; then
 echo 'export JAVA_OPTIONS="$JAVA_OPTIONS '"$RUNTIME_OPTS"'"' >> /var/opt/jfrog/artifactory/etc/default
 fi
+if [ -n "$(grep artdist /var/opt/jfrog/artifactory/etc/default)" ] ; then
+    sed -i s/-Dartdist=rpm/-Dartdist=docker/g /etc/opt/jfrog/artifactory/default
+else
+    echo export JAVA_OPTIONS='"$JAVA_OPTIONS -Dartdist=docker"' >> /var/opt/jfrog/artifactory/etc/default
+fi
 . /etc/init.d/artifactory wait
